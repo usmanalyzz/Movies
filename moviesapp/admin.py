944bf548438d63f8b6b46bbb5dd49fb8a1aed579
usmanalyzz <95336022+usmanalyzz@users.noreply.github.com>
2024-11-04 17:15:10 +0500

    Here is the complete code for movies

---
+++ moviesapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Movie)
+admin.site.register(Video)
+admin.site.register(Screenshot)
+admin.site.register(TechSpec)
+admin.site.register(MovieInfo)
+admin.site.register(Like)
+admin.site.register(Comment)
